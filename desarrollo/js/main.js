fd87ab23b05be9b98718ba5c3864ea03fed261a0
marcelo contreras <marcelojoc@gmail.com>
2016-07-31 17:15:33 -0300

    validacion completa-- verificar campos radio0 y 1

--- desarrollo/js/main.js
+++ desarrollo/js/main.js
@@ -3,6 +3,7 @@ $(function () {
 
     var divCheck = $('#divCheck'); // asigno el elemento a la variable para manipularla despues
     divCheck.css('display', 'none'); // pongo el atributo no visible para todo el elemento
+    $("#alerta").css('display', 'none');
 
     // dejo visible o no el input de la fecha
 		$("#radio-0").click(function(){
@@ -22,7 +23,7 @@ var picker = new Pikaday(
         field: document.getElementById('check'),    
         position: 'bottom right',
         firstDay: 1,
-        format: 'D/M/YYYY',
+        format: 'D/MM/YYYY',
         minDate: new Date(),
         maxDate: new Date(2020, 12, 31),
         yearRange: [2000,2020]
@@ -32,43 +33,49 @@ var picker = new Pikaday(
     // validacion de correo nombre y fecha
 
     $('#form_log').submit(function(e){
+        /*
+        1) traerme los datos que estan en los campos
+        2) comprobar que tengan datos los campos
+        3) verificar si es visitante o huesped
+        4) devilver un verdadero o falso 
+        5) si es falso disparar el setTiemout con el alert.
 
+        */
+        var email= $("#txtmail").val();
+        var nombre = $("#txtnombre").val();
+        var fecha = $("#check").val();
 
-/*
-1) traerme los datos que estan en los campos
-2) comprobar que tengan datos los campos
-3) verificar si es visitante o huesped
-4) devilver un verdadero o falso 
-5) si es falso disparar el setTiemout con el alert.
+            if (email == "" || nombre == "") {
+                
+                    showAlert()
+                    return false
 
- */
-var email= $("#txtmail").val();
-var nombre = $("#txtnombre").val();
+            } else {
+                
+                if($('#radio0').prop('checked')){
 
-if (email == "" || nombre == "") {
-    
-        showAlert()
-        return false
+                    // comprobar que es una fecha el contenido
+                    if(!moment(fecha, 'dd/mm/YYYY').isValid()){
+                        showAlert()
+                        return false
+                    }
 
-} else {
-    
-    
+                }
+                return true;
+            }
 
-
-}
-
-
-});
+        });
 
 });
 
 
 function showAlert(){
 
-        setTimeout(function(){
+    $( "#alerta" ).show()
+    setTimeout(function(){
 
-        $( "#alerta" ).hide( 1000 );
-        }, 2000);
+    $( "#alerta" ).hide();
+    }, 3000);
         
 
 }
